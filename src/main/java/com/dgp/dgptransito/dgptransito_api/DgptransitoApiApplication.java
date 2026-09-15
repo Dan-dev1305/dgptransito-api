7e9745beb164a91082fbf4accc75397c5fbd49ef
unknown <danielgoncalves.paz@gmail.com>
2026-09-14 21:10:38 -0300

    Inicio do projeto de estudo

---
+++ src/main/java/com/dgp/dgptransito/dgptransito_api/DgptransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.dgp.dgptransito.dgptransito_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgptransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgptransitoApiApplication.class, args);
+	}
+
+}
