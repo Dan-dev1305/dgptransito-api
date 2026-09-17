64713eae3e3acf1f7aacfb1f79af911275e25a10
Dan-dev1305 <danielgoncalves.paz@gmail.com>
2026-09-17 13:50:26 -0300

    feat:Adicionado a dependência do lombok e implementado ao controller do proprietário.

--- src/main/java/com/dgp/dgptransito/dgptransito_api/domain/model/Proprietario.java
+++ src/main/java/com/dgp/dgptransito/dgptransito_api/domain/model/Proprietario.java
@@ -1,5 +1,10 @@
 package com.dgp.dgptransito.dgptransito_api.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Proprietario {
 
     private long id;
@@ -7,35 +12,5 @@ public class Proprietario {
     private String email;
     private String telefone;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
